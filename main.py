32c61dcdb3171d6ef03fc37d7c115e35a24d26e7
Qst236 <167086442+Qst236@users.noreply.github.com>
2024-09-22 01:05:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import io
 import re
+import gzip
 import requests
 
 new_data = []
@@ -19,3 +21,22 @@ new_data.append(china[0])
 
 f = open("IPV4.m3u", "w")
 f.write("\n\n".join(new_data))
+
+# Chinese EPG
+china_epg = requests.get("https://raw.githubusercontent.com/BurningC4/Chinese-IPTV/refs/heads/master/guide.xml").text
+china_epg = china_epg.splitlines()[:-1]
+china_epg = '\n'.join(china_epg)
+
+# Indonesia EPG
+indonesia = requests.get('https://s.urfan.web.id/epg', stream=True)
+with gzip.open(io.BytesIO(indonesia.content), 'rb') as f_in:
+    with open('guide.xml', 'wb') as f_out:
+        f_out.write(f_in.read())
+
+with open("guide.xml", "r") as f:
+	indonesia = f.read()
+	indonesia = indonesia.splitlines()[2:]
+	indonesia = '\n'.join(indonesia)
+
+with open("guide.xml", "w") as f:
+	f.write(china_epg + indonesia)
